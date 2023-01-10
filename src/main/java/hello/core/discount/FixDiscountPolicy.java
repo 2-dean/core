90b51abb83d34534c427ea963f3348488c0906d2
2-dean <limsjs@naver.com>
2023-01-11 00:07:45 +0900

    빈 생명주기 콜백 ing

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -4,10 +4,10 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
-    private  int discountFixAmoun = 1000; //1000원 할인
+    private final int discountFixAmoun = 1000; //1000원 할인
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
--- src/test/java/hello/core/order/OrderServiceImplTest.java
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -2,15 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
+
+    OrderServiceImpl orderService;
     @Test
     void createOrder() {
-        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
+
+        MemberServiceImpl memberService = new MemberServiceImpl(memoryMemberRepository);
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memoryMemberRepository, new FixDiscountPolicy());
 
         orderService.createOrder(1L, "itemA", 10000);
     }
